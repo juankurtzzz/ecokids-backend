ca8e7f55d449af65d04631f25eb21d2d9e574af9
juankurtzzz <juanpietro1@outlook.com>
2026-04-16 01:56:26 -0300

    Removendo regras do gitignore

--- src/drizzle/0000_volatile_bill_hollister.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "criancas" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"nome_crianca" varchar(100) NOT NULL,
-	"numero_embalagens" integer DEFAULT 0 NOT NULL,
-	"numero_moedas" integer DEFAULT 0 NOT NULL,
-	"escola" varchar(150) NOT NULL,
-	"serie" varchar(20) NOT NULL
-);
--- src/drizzle/0001_hard_ultimates.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "user" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"email" varchar(150) NOT NULL,
-	"password" varchar(150) NOT NULL
-);
